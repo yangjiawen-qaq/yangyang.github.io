c64cafee9e9698a11ecf5b5b24fce0e8a571cb92
yangjiawen-qaq <2627670956@qq.com>
2020-05-22 12:07:16 +0800

    travel

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<home-header></home-header>
+</div>
+</template>
+<script>
+import HomeHeader from './components/homeheader'
+export default{
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style>
+</style>
---
+++ travel/src/pages/home/components/homeheader.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+<div class="header">
+  <div class="header-left">
+    <div class="iconfont icon-left">&#xe6bc;</div>
+  </div>
+  <div class="header-input">
+     <span class="iconfont">&#xe647;</span>
+    请输入城市景点/地区</div>
+  <div class="header-right">
+    城市
+    <span class="iconfont">&#xe60a;</span>
+  </div>
+</div>
+</div>
+</template>
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/variable.styl'
+.header
+{
+  height:.86rem;
+  background :$Bgcolor;
+  display :flex;
+  line-height .86rem;
+  color:#fff;
+}
+.header-left
+{
+ width :.64rem;
+ float:left;
+ .icon-left
+ {
+   font-size:.4rem;
+   text-align :center;
+ }
+}
+ .header-right
+ {
+  width :1.23rem;
+  float:right;
+  text-align :center;
+ }
+ .header-input
+ {
+   flex:1;
+   background :#fff;
+   padding-left :.1rem;
+   height .64rem;
+   color:#ccc;
+   line-height .64rem;
+   border-radius:.1rem;
+   margin:.12rem;
+ }
+</style>
